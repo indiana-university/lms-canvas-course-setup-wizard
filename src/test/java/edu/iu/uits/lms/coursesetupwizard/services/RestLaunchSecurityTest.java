a34f147c25054f5382915dfab1f0e16897b9df77
chmaurer <chmaurer@iu.edu>
2022-08-08 10:49:10 -0400

    Add license headers

--- src/test/java/edu/iu/uits/lms/coursesetupwizard/services/RestLaunchSecurityTest.java
+++ src/test/java/edu/iu/uits/lms/coursesetupwizard/services/RestLaunchSecurityTest.java
@@ -1,5 +1,38 @@
 package edu.iu.uits.lms.coursesetupwizard.services;
 
+/*-
+ * #%L
+ * course-setup-wizard
+ * %%
+ * Copyright (C) 2022 Indiana University
+ * %%
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the Indiana University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
 import edu.iu.uits.lms.coursesetupwizard.config.ToolConfig;
 import edu.iu.uits.lms.coursesetupwizard.repository.WizardCourseStatusRepository;
 import edu.iu.uits.lms.coursesetupwizard.repository.WizardUserCourseRepository;
@@ -100,7 +133,7 @@ public class RestLaunchSecurityTest {
 
    @Test
    public void restNoAuthnOpenEndpointPost() throws Exception {
-      //This is an unsecured endpoint and should allow access without authn
+      //This is an unsecured endpoint and should allow access without authn, but still needs csrf
       SecurityContextHolder.getContext().setAuthentication(null);
       mvc.perform(post("/rest/popup/1234/asdf/dismiss").with(csrf())
                   .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
@@ -110,7 +143,7 @@ public class RestLaunchSecurityTest {
 
    @Test
    public void restNoAuthnOpenEndpointMissingCsrfPost() throws Exception {
-      //This is an unsecured endpoint and should allow access without authn
+      //This is an unsecured endpoint and should allow access without authn, but still needs csrf
       SecurityContextHolder.getContext().setAuthentication(null);
       mvc.perform(post("/rest/popup/1234/asdf/dismiss")
                   .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
