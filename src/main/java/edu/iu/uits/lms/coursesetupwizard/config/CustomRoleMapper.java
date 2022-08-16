26c093a182e1407b3763e3c5890a5f5293e0199d
Gregory J Thomas <gjthomas@iu.edu>
2022-08-16 14:16:10 -0400

    LMSA-8509 - adjust permissions

--- src/main/java/edu/iu/uits/lms/coursesetupwizard/config/CustomRoleMapper.java
+++ src/main/java/edu/iu/uits/lms/coursesetupwizard/config/CustomRoleMapper.java
@@ -33,7 +33,7 @@ package edu.iu.uits.lms.coursesetupwizard.config;
  * #L%
  */
 
-import edu.iu.uits.lms.canvas.helpers.CanvasConstants;
+import edu.iu.uits.lms.canvas.helpers.EnrollmentHelper;
 import edu.iu.uits.lms.lti.LTIConstants;
 import edu.iu.uits.lms.lti.repository.DefaultInstructorRoleRepository;
 import edu.iu.uits.lms.lti.service.LmsDefaultGrantedAuthoritiesMapper;
@@ -53,20 +53,20 @@ public class CustomRoleMapper extends LmsDefaultGrantedAuthoritiesMapper {
     protected String returnEquivalentAuthority(String[] userRoles, List<String> instructorRoles) {
         List<String> userRoleList = Arrays.asList(userRoles);
 
-        // let TAs use the tool
-        if (userRoleList.contains(CanvasConstants.TA_ROLE)) {
-            return LTIConstants.INSTRUCTOR_AUTHORITY;
-        }
-
-        // let Designers use the tool
-        if (userRoleList.contains(CanvasConstants.DESIGNER_ROLE)) {
-            return LTIConstants.INSTRUCTOR_AUTHORITY;
-        }
-
         for (String instructorRole : instructorRoles) {
             if (userRoleList.contains(instructorRole)) {
                 return LTIConstants.INSTRUCTOR_AUTHORITY;
             }
+        }
+
+        // let TAs use the tool
+        if (userRoleList.contains(EnrollmentHelper.TYPE_TA)) {
+            return LTIConstants.INSTRUCTOR_AUTHORITY;
+        }
+
+        // let Designers use the tool
+        if (userRoleList.contains(EnrollmentHelper.TYPE_DESIGNER)) {
+            return LTIConstants.INSTRUCTOR_AUTHORITY;
         }
 
         return LTIConstants.STUDENT_AUTHORITY;
