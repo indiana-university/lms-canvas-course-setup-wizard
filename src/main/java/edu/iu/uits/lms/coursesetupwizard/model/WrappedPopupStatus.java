4363c1b0ce2f382c0a37a8319db0e68207d4aa54
chmaurer <chmaurer@iu.edu>
2022-10-06 11:03:52 -0400

    LMSA-8453 - use the lombok constructor

--- src/main/java/edu/iu/uits/lms/coursesetupwizard/model/WrappedPopupStatus.java
+++ src/main/java/edu/iu/uits/lms/coursesetupwizard/model/WrappedPopupStatus.java
@@ -1,18 +1,15 @@
 package edu.iu.uits.lms.coursesetupwizard.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class WrappedPopupStatus implements Serializable {
 
    private PopupStatus popupStatus;
    private String csrfToken;
-
-   public WrappedPopupStatus(PopupStatus popupStatus, String csrfToken) {
-      this.popupStatus = popupStatus;
-      this.csrfToken = csrfToken;
-   }
 
 }
