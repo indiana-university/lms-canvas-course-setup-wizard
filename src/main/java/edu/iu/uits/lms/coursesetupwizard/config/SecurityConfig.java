a5960725035d69d7bbaaf408bfbf190c86ed5cec
Damian Sobieralski <dsobiera@iu.edu>
2024-09-18 17:15:34 -0400

    LMSA-8946 - moar updates!

--- src/main/java/edu/iu/uits/lms/coursesetupwizard/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/coursesetupwizard/config/SecurityConfig.java
@@ -36,11 +36,8 @@ package edu.iu.uits.lms.coursesetupwizard.config;
 import edu.iu.uits.lms.common.it12logging.LmsFilterSecurityInterceptorObjectPostProcessor;
 import edu.iu.uits.lms.common.it12logging.RestSecurityLoggingConfig;
 import edu.iu.uits.lms.common.oauth.CustomJwtAuthenticationConverter;
-import edu.iu.uits.lms.iuonly.services.DeptProvisioningUserServiceImpl;
-import edu.iu.uits.lms.lti.LTIConstants;
 import edu.iu.uits.lms.lti.repository.DefaultInstructorRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -56,41 +53,11 @@ import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWrite
 import uk.ac.ox.ctl.lti13.Lti13Configurer;
 
 import static edu.iu.uits.lms.lti.LTIConstants.INSTRUCTOR_AUTHORITY;
-import static edu.iu.uits.lms.lti.LTIConstants.INSTRUCTOR_ROLE;
 import static edu.iu.uits.lms.lti.LTIConstants.WELL_KNOWN_ALL;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-//    @Configuration
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER - 4)
-//    public static class RestSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
-//
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//            http
-//                  .cors().and()
-//                    .requestMatchers().antMatchers("/api/**", "/rest/**")
-//                    .and()
-//                    .authorizeRequests()
-//                    .antMatchers("/api/**").permitAll()
-//                    .antMatchers("/rest/popup/**").permitAll()
-////                    .access("hasAuthority('SCOPE_lms:edsdev') or (hasAuthority('SCOPE_lms:rest') and hasAuthority('ROLE_LMS_REST_ADMINS'))")
-//                    .antMatchers("/rest/**").access("hasAuthority('SCOPE_lms:rest') and hasAuthority('ROLE_LMS_REST_ADMINS')")
-//                    .and()
-//                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                    .and()
-//                    .oauth2ResourceServer()
-//                    .jwt().jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
-//
-//            http.apply(new RestSecurityLoggingConfig());
-//
-//            //Disable csrf for the popup endpoints
-//            http.csrf().ignoringAntMatchers("/rest/popup/**");
-//        }
-//    }
-
     @Order(4)
     @Bean
     public SecurityFilterChain restFilterChain(HttpSecurity http) throws Exception {
@@ -117,59 +84,7 @@ public class SecurityConfig {
         return http.build();
     }
 
-//
-//    @Configuration
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER - 3)
-//    public static class AppSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//
-//        @Autowired
-//        private DefaultInstructorRoleRepository defaultInstructorRoleRepository;
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http
-//                  .requestMatchers()
-//                  .antMatchers(WELL_KNOWN_ALL, "/error", "/app/**", "/tool/**")
-//                  .and()
-//                  .authorizeRequests()
-//                  .antMatchers(WELL_KNOWN_ALL, "/error").permitAll()
-//                  .antMatchers("/app/**", "/tool/**").hasRole(LTIConstants.INSTRUCTOR_ROLE)
-//                  .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
-//                  .and()
-//                  .headers()
-//                  .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
-//                  .and()
-//                  .referrerPolicy(referrer -> referrer
-//                          .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
-//
-//            //Setup the LTI handshake
-//            Lti13Configurer lti13Configurer = new Lti13Configurer()
-//                    .grantedAuthoritiesMapper(new CustomRoleMapper(defaultInstructorRoleRepository));
-//
-//            http.apply(lti13Configurer);
-//
-//            //Fallback for everything else
-//            http.requestMatchers().antMatchers("/**")
-//                    .and()
-//                    .authorizeRequests()
-//                    .anyRequest().authenticated()
-//                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
-//                    .and()
-//                    .headers()
-//                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
-//                    .and()
-//                    .referrerPolicy(referrer -> referrer
-//                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
-//        }
-//
-//        @Override
-//        public void configure(WebSecurity web) throws Exception {
-//            // ignore everything except paths specified
-//            web.ignoring().antMatchers("/app/jsrivet/**", "/app/jsreact/**", "/app/webjars/**",
-//                  "/app/css/**", "/app/js/**", "/app/images/**", "/favicon.ico");
-//        }
-
-    @Order(6)
+    @Order(3)
     @Bean
     public SecurityFilterChain appFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher(WELL_KNOWN_ALL, "/error", "/app/**", "/tool/**")
@@ -199,7 +114,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain catchallFilterChain(HttpSecurity http) throws Exception {
-        //Setup the LTI handshake
+        // Setup the LTI handshake
         http.with(new Lti13Configurer(), lti ->
                 lti.setSecurityContextRepository(new HttpSessionSecurityContextRepository())
                         .grantedAuthoritiesMapper(new CustomRoleMapper(defaultInstructorRoleRepository)));
