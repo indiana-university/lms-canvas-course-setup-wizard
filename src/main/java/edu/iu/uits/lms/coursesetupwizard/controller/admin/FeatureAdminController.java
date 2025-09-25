080400c4c6da19cfd87baf503a68aa871247c076
Gregory J Thomas <gjthomas@iu.edu>
2025-09-25 11:12:06 -0400

    LMSA-9572 - updating the headers and adding headers in some cases

--- src/main/java/edu/iu/uits/lms/coursesetupwizard/controller/admin/FeatureAdminController.java
+++ src/main/java/edu/iu/uits/lms/coursesetupwizard/controller/admin/FeatureAdminController.java
@@ -4,7 +4,7 @@ package edu.iu.uits.lms.coursesetupwizard.controller.admin;
  * #%L
  * course-setup-wizard
  * %%
- * Copyright (C) 2022 Indiana University
+ * Copyright (C) 2022 - 2025 Indiana University
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -15,7 +15,7 @@ package edu.iu.uits.lms.coursesetupwizard.controller.admin;
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * 3. Neither the name of the Indiana University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
